34ac014fcd4cfa672df4251d1534bfdc2defc718
AlenaZavadskaya <helen.zavadskaya@yandex.ru>
2021-01-19 10:48:26 +0300

    get current user (/users/me)

--- controllers/users.js
+++ controllers/users.js
@@ -1,23 +1,27 @@
-const User = require('../models/user');
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
 
 module.exports.getUsers = (req, res) => {
   User.find({})
     .then((users) => {
       res.status(200).send({ data: users });
     })
-    .catch((err) => res.status(500).send({ message: `Произошла ошибка: ${err}` }));
+    .catch((err) =>
+      res.status(500).send({ message: `Произошла ошибка: ${err}` })
+    );
 };
 
 module.exports.getUserById = (req, res) => {
   User.findById(req.params._id)
     .then((user) => {
       if (!user) {
-        return res.status(404).send({ message: 'Нет пользователя с таким id' });
+        return res.status(404).send({ message: "Нет пользователя с таким id" });
       }
       return res.status(200).send({ data: user });
     })
     .catch((err) => {
-      if (err.name === 'CastError') {
+      if (err.name === "CastError") {
         res.status(400).send({ message: `Ошибка валидации: ${err}` });
       } else {
         res.status(500).send({ message: `Произошла ошибка: ${err}` });
@@ -25,15 +29,59 @@ module.exports.getUserById = (req, res) => {
     });
 };
 
-module.exports.createUser = (req, res) => {
-  const { name, about, avatar, email, password } = req.body;
-  User.create({ name, about, avatar, email, password })
-    .then((users) => res.status(200).send({ data: users }))
+module.exports.getCurrentUser = (req, res, next) => {
+  User.findById(req.user._id)
+    .then((user) => {
+      console.log("user", user);
+      res.status(200).send(user);
+    })
+    // .catch(next);
     .catch((err) => {
-      if (err.name === 'ValidationError') {
-        res.status(400).send({ message: `Некорректно указаны данные пользователя: ${err}`  });
+      if (err.name === "CastError") {
+        res.status(400).send({ message: `Ошибка валидации: ${err}` });
       } else {
         res.status(500).send({ message: `Произошла ошибка: ${err}` });
       }
+    });
+};
+
+module.exports.createUser = (req, res, next) => {
+  const { name, about, avatar, email, password } = req.body;
+  User.findOne({ email })
+    .then((user) => {
+      if (user) {
+        // throw new ConflictError('This user already exists');
+      }
+      return bcrypt.hash(password, 10);
+    })
+    .then((hash) =>
+      User.create({
+        email,
+        password: hash,
+        name,
+        about,
+        avatar,
+      })
+    )
+    .then((user) => res.status(200).send(user))
+    .catch(next);
+};
+
+module.exports.login = (req, res) => {
+  const { email, password } = req.body;
+
+  return User.findUserByCredentials(email, password)
+    .then((user) => {
+      res.send({
+        token: jwt.sign({ _id: user._id }, "some-secret-key", {
+          expiresIn: "7d",
+        }),
+      });
+      console.log("user", req.user);
+      console.log("user1", user);
+    })
+    .catch((err) => {
+      // ошибка аутентификации
+      res.status(401).send({ message: err.message });
     });
 };
