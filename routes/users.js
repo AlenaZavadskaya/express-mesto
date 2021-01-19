0ce6b3cd576bdfbabb3fb248c4b380831e76a8c3
AlenaZavadskaya <helen.zavadskaya@yandex.ru>
2021-01-19 10:49:42 +0300

    get current user (/users/me)

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,13 @@
 const router = require('express').Router();
-const { getUsers, getUserById, createUser } = require('../controllers/users');
+const { getUsers, getUserById, createUser, login, getCurrentUser } = require('../controllers/users');
 
+
+// router.post('/signup', createUser);
+// router.post('/signin', login);
 router.get('/users', getUsers);
+router.get('/users/me', getCurrentUser);
 router.get('/users/:_id', getUserById);
+
 router.post('/users', createUser);
 
 module.exports = router;
