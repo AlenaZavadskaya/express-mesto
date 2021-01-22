312ac824188127413069503c09477e04c11bd3e4
AlenaZavadskaya <helen.zavadskaya@yandex.ru>
2021-01-22 16:32:20 +0300

    add allowed cors

--- app.js
+++ app.js
@@ -24,7 +24,24 @@ mongoose.connect("mongodb://localhost:27017/mestodb", {
   useUnifiedTopology: true,
 });
 
+// Массив разешённых доменов
+const allowedCors = [
+  'https://AlenaZavadskaya.students.nomoreparties.space',
+  'https://www.AlenaZavadskaya.students.nomoreparties.space',
+  'localhost:3000'
+];
+
 app.use(cors());
+
+app.use(function(req, res, next) {
+  const { origin } = req.headers; // Записываем в переменную origin соответствующий заголовок
+
+  if (allowedCors.includes(origin)) { // Проверяем, что значение origin есть среди разрешённых доменов
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+
+  next();
+});
 
 app.use(requestLogger);
 
