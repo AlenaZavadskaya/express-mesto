89630471bc5aee9405c28ab5b6bc740c388ab1ff
AlenaZavadskaya <helen.zavadskaya@yandex.ru>
2021-01-26 20:25:57 +0300

    add conflict error for repeated email

--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,10 @@
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const User = require("../models/user");
-const NotFoundError = require("../errors/not-found-err");
-const BadRequestError = require("../errors/bad-request-err");
-const UnauthorizedError = require("../errors/unauthorized-err");
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+const NotFoundError = require('../errors/not-found-err');
+const BadRequestError = require('../errors/bad-request-err');
+const UnauthorizedError = require('../errors/unauthorized-err');
+const ConflictError = require('../errors/conflict-error');
 require('dotenv').config();
 
 const { NODE_ENV, JWT_SECRET } = process.env;
@@ -18,11 +19,11 @@ module.exports.getUsers = (req, res, next) => {
 
 module.exports.getUserById = (req, res, next) => {
   User.findById(req.params._id)
-    .orFail(() => new NotFoundError("Нет пользователя с таким id"))
+    .orFail(() => new NotFoundError('Нет пользователя с таким id'))
     .then((user) => res.status(200).send({ data: user }))
     .catch((err) => {
-      if (err.kind === "ObjectId") {
-        throw new BadRequestError("Ошибка валидации: неверный id пользователя");
+      if (err.kind === 'ObjectId') {
+        throw new BadRequestError('Ошибка валидации: неверный id пользователя');
       }
       return next(err);
     })
@@ -31,7 +32,7 @@ module.exports.getUserById = (req, res, next) => {
 
 module.exports.getCurrentUser = (req, res, next) => {
   User.findById(req.user._id)
-    .orFail(() => new NotFoundError("Нет пользователя с таким id"))
+    .orFail(() => new NotFoundError('Нет пользователя с таким id'))
     .then((user) => res.status(200).send(user))
     .catch((err) => {
       next(err);
@@ -39,30 +40,27 @@ module.exports.getCurrentUser = (req, res, next) => {
 };
 
 module.exports.createUser = (req, res, next) => {
-  const { name, about, avatar, email, password } = req.body;
+  const {
+    name, about, avatar, email, password,
+  } = req.body;
   User.findOne({ email })
     .then((user) => {
       if (user) {
-        throw new BadRequestError("Пользователь с данным email уже существует");
+        throw new ConflictError('Пользователь с данным email уже существует');
       }
       return bcrypt.hash(password, 10);
     })
-    .then((hash) =>
-      User.create({
-        email,
-        password: hash,
-        name,
-        about,
-        avatar,
-      })
-    )
+    .then((hash) => User.create({
+      email,
+      password: hash,
+      name,
+      about,
+      avatar,
+    }))
     .then((user) => {
+      // eslint-disable-next-line no-shadow
       User.findById(user._id).then((user) => res.status(200).send(user));
     })
-    // .then((user) => {
-    //   const { _id, email } = user;
-    //   res.send({ data: { _id, email } })
-    // })
     .catch(next);
 };
 
@@ -72,14 +70,16 @@ module.exports.login = (req, res, next) => {
   return User.findUserByCredentials(email, password)
     .then((user) => {
       res.status(200).send({
-        // token: jwt.sign({ _id: user._id }, "some-secret-key", {
-        //   expiresIn: "7d",
-        token: jwt.sign({ _id: user._id }, NODE_ENV === "production" ? JWT_SECRET : "some-secret-key", {
-          expiresIn: "7d",
-        }),
+        token: jwt.sign(
+          { _id: user._id },
+          NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key',
+          {
+            expiresIn: '7d',
+          },
+        ),
       });
     })
     .catch(() => {
-      next(new UnauthorizedError("Неправильные почта или пароль"));
+      next(new UnauthorizedError('Неправильные почта или пароль'));
     });
 };
